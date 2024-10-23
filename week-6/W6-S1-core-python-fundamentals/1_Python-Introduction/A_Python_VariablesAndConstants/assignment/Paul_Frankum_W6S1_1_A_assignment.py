ac180246385b3612086d0a712366ca6678469ac4
PaulFrankum <paul_frankum@msn.com>
2024-10-23 04:07:44 +0100

    week 6updates

---
+++ week-6/W6-S1-core-python-fundamentals/1_Python-Introduction/A_Python_VariablesAndConstants/assignment/Paul_Frankum_W6S1_1_A_assignment.py
@@ -0,0 +1,25 @@
+# Start writing code with comments form here
+
+# Const Pi to 8 places float and immutable 
+PI = 3.1415927 # Decimal numbers
+print("Pi to 8 Places ",PI)
+
+# interger and immutable
+year_of_birth = 1970
+print("My year of Birth is ",year_of_birth)
+
+# boolean mutable
+happy = True 
+print(" I am Happy with this course ", happy)
+
+#  string imutable
+my_name = "Paul Matthew Frankum" 
+print(" My full name is ", my_name)
+
+# interger and mutable
+age=54
+print(" my age is ",age)
+
+# Mutable example with a list
+cars = ["ford", "nissan", "land rover"]
+print("Original List:", cars)
---
+++ week-6/W6-S1-core-python-fundamentals/2_Python-KeyDataStructures-P1/B_PythonTuples/arrays.py
@@ -0,0 +1,15 @@
+my_array = ["apple", "banana", "cherry", "date"]
+
+print("print first element and second list starts from 0 ",my_array[0]," ",my_array[2])
+
+my_array[1] = "blueberry"
+print("Banana swapped with blueberry ",my_array)
+
+print("print element 2 to 4 ",my_array[1:4])
+
+my_array.append("elderberry")
+print("added elderberry to end of list ",  my_array)
+
+my_array.remove("date")
+print("removed date from list ", my_array)
+
