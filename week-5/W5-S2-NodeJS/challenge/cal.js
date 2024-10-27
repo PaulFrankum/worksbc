f2c4ecf100f33fb0bb12a7995936728084bb930d
PaulFrankum <paul_frankum@msn.com>
2024-10-27 15:32:50 +0000

    w5 s2 daily challenger done

---
+++ week-5/W5-S2-NodeJS/challenge/cal.js
@@ -0,0 +1,58 @@
+// switch  + - * / invlaid operator and call function 
+const {add} = require("./add.js");
+const {subtract} = require("./subtract.js");
+const {multiply} = require("./multiply.js");
+const {divide} = require("./divide.js");
+const fs = require("fs");
+const path = require("path");
+var sum = 0
+var result = 0
+
+// input numbers
+const firstNumber = Number(process.argv[2])
+var operator = process.argv[3]
+const secondNumber = Number(process.argv[4])
+
+// check first number
+if (typeof(firstNumber) == "NaN"){
+  console.log("First Number not a number")
+}
+// check second number
+if (typeof(secondNumber) == "NaN"){
+  console.log("Second Number not a number")
+}
+
+// switch  + - * / invalid operator and call function 
+  switch(operator) {
+    case "+":
+      sum = (add(firstNumber,secondNumber))
+      break;
+
+    case "-": 
+      sum = (subtract(firstNumber,secondNumber))
+      break;
+ 
+    case "t":
+      sum = (multiply(firstNumber,secondNumber))
+      operator = "*"
+      break;
+ 
+    case "d":
+      sum = (divide(firstNumber,secondNumber))
+      operator = "/"
+      break;
+ 
+    default:
+      sum = "Incorrect operator!"
+      console.log("Incorrect operator!")
+  }
+
+// print result
+result = firstNumber + " "+ operator + " " + secondNumber + " = " + sum +'\n'
+console.log(result)
+
+// save to log file
+fs.appendFile("calculator.txt", result, (err) => { 
+  if (err) throw err;
+    console.log("file written successfully!")});
+
